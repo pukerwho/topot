48883684218d3b69cfdecae373516c13d99d7b7f
Vladimir Samojlov <v-moi@ya.ru>
2021-01-11 15:14:17 +0200

    edit

--- blocks/main/hey.php
+++ blocks/main/hey.php
@@ -2,14 +2,14 @@
 	<div class="container mx-auto mx-auto px-4 lg:px-0">
 		<h2 class="text-center text-4xl md:text-5xl mb-6"><?php _e('Готовы начать общение?'); ?></h2>
 		<div class="w-full lg:w-5/6 mx-auto text-left md:text-center text-xl mb-6">
-			Работаем с самыми разными задачами: от создания простого лендинга за два дня, до подключения и синхронизации 1С с вашим интернет-магазином. Мы выполняем работу, как для частного лица, так и для крупной компании. 
+			<?php _e('Работаем с самыми разными задачами: от создания простого лендинга за два дня, до подключения и синхронизации 1С с вашим интернет-магазином. Мы выполняем работу, как для частного лица, так и для крупной компании.', 'topot'); ?>
 		</div>
 		<div class="flex flex-col text-center md:flex-row md:justify-center text-2xl second-color mx-auto">
 			<div class="mr-0 md:mr-20">
-				<a href="tel:">099-099-99-99</a>
+				<a href="tel:+380997713997">+380-99-771-3997</a>
 			</div>
 			<div>
-				<a href="mailto:">info@timeto.top</a>
+				<a href="mailto:hello@treba-solutions.com">hello@treba-solutions.com</a>
 			</div>
 		</div>
 	</div>
