3c9c754aab3f6edbc828e68238715c51ab76015b
Vladimir Samojlov <v-moi@ya.ru>
2021-01-27 10:48:43 +0200

    edit

--- archive-services.php
+++ archive-services.php
@@ -11,13 +11,15 @@
 			)); 
 			foreach ($services as $service): ?>
 				<div class="services_item w-full lg:w-1/3 block lg:px-2 mb-4">
-					<div class="bg-light rounded-lg flex flex-col justify-center p-6">
+					<div class="h-full bg-light rounded-lg flex flex-col justify-between p-6">
 						<div class="mb-6">
-							<img src="<?php echo carbon_get_term_meta($service->term_id, 'crb_uslugi_thumb'); ?>" alt="<?php echo $service->name ?>" width="60">
+							<div class="mb-6">
+								<img src="<?php echo carbon_get_term_meta($service->term_id, 'crb_uslugi_thumb'); ?>" alt="<?php echo $service->name ?>" width="60">
+							</div>
+			      	<div class="title text-white text-2xl">
+			      		<?php echo $service->name; ?>
+			      	</div>
 						</div>
-		      	<div class="title text-white text-2xl mb-6">
-		      		<?php echo $service->name; ?>
-		      	</div>
 		      	<div>
 		      		<a href="<?php echo get_term_link($service); ?>">
 		      			<?php _e('Перейти', 'topot'); ?> →
