a4e83d945828825b20c8c5ff04c4cbbed404dd8a
Vladimir Samojlov <v-moi@ya.ru>
2021-02-19 12:34:40 +0200

    Edit

--- archive-portfolio.php
+++ archive-portfolio.php
@@ -3,23 +3,23 @@
 
 <div class="pt-32 pb-20">
 	<div class="container mx-auto px-2 lg:px-0">
-		<h1 class="text-5xl first-color text-center mb-12"><?php echo post_type_archive_title(); ?></h1>
-		<div class="flex flex-wrap lg:-mx-2">
+		<div class="mb-12">
+			<h1 class="w-1/2 text-3xl lg:text-5xl first-color"><?php echo post_type_archive_title(); ?></h1>
+			<div class="text-2xl"><?php _e('Сайты, разработанные нашей студией', 'treba'); ?></div>	
+		</div>
+		<div class="flex flex-wrap lg:-mx-2 mt-20">
 			<?php 
 				$services_query = new WP_Query(array(
 					'post_type' => 'portfolio',
-					'posts_per_page' => 9,
+					'posts_per_page' => 8,
 				)); 
 				if ($services_query->have_posts()) : while ($services_query->have_posts()) : $services_query->the_post(); 
 			?>
-				<a href="<?php the_permalink(); ?>" class="w-full lg:w-1/3 block lg:px-2 mb-4">
-					<div>
-						<img src="<?php echo carbon_get_the_post_meta('crb_portfolio_thumb'); ?>" alt="<?php the_title(); ?>" class="w-full object-cover object-top" style="height: 275px;">
-					</div>
-					<div class="bg-light rounded-b-lg text-center py-8 px-2">
-						<span class="text-lg text-white opacity-75"><?php echo carbon_get_the_post_meta('crb_portfolio_title'); ?></span>
-					</div>
-				</a>
+				<div class="w-1/2 overflow-hidden px-2 service_example_item animate-puk mb-4">
+					<a href="<?php the_permalink(); ?>">
+						<img src="<?php echo get_the_post_thumbnail_url(get_the_ID(), 'large') ?>" alt="" class="w-full">
+					</a>
+				</div>
 			<?php endwhile; endif; wp_reset_postdata(); ?>
 		</div>
 	</div>
