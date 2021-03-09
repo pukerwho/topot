e2272241798c76a019c5051ab01cbddc60650925
Vladimir Samojlov <v-moi@ya.ru>
2021-03-09 10:58:29 +0200

    Edit

--- header.php
+++ header.php
@@ -2,16 +2,21 @@
 <html <?php language_attributes(); ?> class="no-js no-svg">
 
 <head>
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-148889633-1"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-148889633-1');
+  </script>
+
   <meta charset="<?php bloginfo( 'charset' ); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
   <!--[if IE]><meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"><![endif]-->
   <link rel="profile" href="http://gmpg.org/xfn/11">
-  <base href="/">
-  <link rel="alternate" hreflang="x-default" href="<?php echo home_url(); ?>">
-  <link rel="alternate" hreflang="en" href="<?php echo home_url(); ?>/en">
-  <link rel="alternate" hreflang="ru" href="<?php echo home_url(); ?>/ru">
-  <link rel="alternate" hreflang="ua" href="<?php echo home_url(); ?>/ua">
   <?php
   // ENQUEUE your css and js in inc/enqueues.php
 
