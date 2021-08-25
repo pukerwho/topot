484821a1cfdd64a4ac95cbbd7646f63b9d3ecd85
Vladimir Samojlov <v-moi@ya.ru>
2021-08-25 21:54:15 +0300

    edit

--- single.php
+++ single.php
@@ -2,14 +2,16 @@
 
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 	<div class="pt-40 pb-20">
-		<div class="container mx-auto">
-			<h1 class="text-3xl lg:text-5xl text-center font-bold mb-6"><?php the_title(); ?></h1>
-			<div class="w-full lg:w-4/5 page bg-light shadow-md rounded-lg my-0 mx-auto lg:my-8 px-4 lg:px-8 py-8 lg:py-10">
-				<article class="content">
-					<?php the_content(); ?>
-				</article>
-			</div>
-		</div>
+		<div class="container mx-auto" itemscope itemtype="http://schema.org/Article">
+			<article class="content">
+				<h1 class="w-full lg:w-4/5 text-3xl lg:text-5xl text-center font-bold mb-6 mx-auto" itemprop="headline"><?php the_title(); ?></h1>
+				<div class="w-full lg:w-4/5 page bg-light shadow-md rounded-lg my-0 mx-auto lg:my-8 px-4 lg:px-8 py-8 lg:py-10">
+					<div itemprop="articleBody">
+						<?php the_content(); ?>	
+					</div>
+				</div>
+			</article>
+		</div> 
 	</div>
 <?php endwhile; else: ?>
 	<p><?php _e('Ничего не найдено'); ?></p>
